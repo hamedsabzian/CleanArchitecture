e253109fb03cceb9175f3adf2c536a99a277e86b
Hamed Sabzian <h.sabziyan@danatadbir.ir>
2025-04-22 17:56:00 +0330

    use id generator instead of direct id generation

---
+++ Todo.Application/Common/IdGenerator.cs
@@ -0,0 +1,14 @@
+﻿namespace Todo.Application.Common;
+
+public interface IIdGenerator
+{
+    public Guid New<TEntity>();
+}
+
+public class IdGenerator : IIdGenerator
+{
+    public Guid New<TEntity>()
+    {
+        return Guid.NewGuid();
+    }
+}
---
+++ Todo.Application/DependencyRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Todo.Application.Common;
+
+namespace Todo.Application;
+
+public static class DependencyRegistration
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddSingleton<IIdGenerator, IdGenerator>();
+        services.AddSingleton<TimeProvider>(TimeProvider.System);
+
+        services.AddMediator(options => { options.ServiceLifetime = ServiceLifetime.Scoped; });
+        return services;
+    }
+}
