aed47e10453e92217ff268303768c14924c6fb7b
Hamed Sabzian <h.sabziyan@danatadbir.ir>
2025-04-23 03:26:09 +0330

    write unit test for pipeline behaviors

---
+++ Tests/Todo.Application.UnitTests/PipelineBehaviors/ExceptionHandlingBehaviorTests.cs
@@ -0,0 +1,36 @@
+﻿using Mediator;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Todo.Application.Common.Behaviors;
+using Todo.Application.Common.ResponseModels;
+
+namespace Todo.Application.UnitTests.PipelineBehaviors;
+
+public class ExceptionHandlingBehaviorTests
+{
+    [Fact]
+    public async Task ShouldBeSuccessful()
+    {
+        var logger = new Mock<ILogger<ExceptionHandlingBehavior<FooRequest, Response>>>().Object;
+        var behavior = new ExceptionHandlingBehavior<FooRequest, Response>(logger);
+
+        var result = await behavior.Handle(
+            new FooRequest(), CancellationToken.None, (_, _) => ValueTask.FromResult(new Response().Success()));
+
+        result.StatusCode.ShouldBe(DefaultResponseStatusCodes.Ok);
+    }
+
+    [Fact]
+    public async Task WithThrowException_ShouldReturnError()
+    {
+        var logger = new Mock<ILogger<ExceptionHandlingBehavior<FooRequest, Response>>>().Object;
+        var behavior = new ExceptionHandlingBehavior<FooRequest, Response>(logger);
+
+        var result = await behavior.Handle(
+            new FooRequest(), CancellationToken.None, (_, _) => throw new InvalidOperationException());
+
+        result.StatusCode.ShouldBe(DefaultResponseStatusCodes.Error);
+    }
+
+    public class FooRequest : IRequest<Response>;
+}
---
+++ Tests/Todo.Application.UnitTests/PipelineBehaviors/ValidationBehaviorTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation;
+using Mediator;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Todo.Application.Common.Behaviors;
+using Todo.Application.Common.ResponseModels;
+
+namespace Todo.Application.UnitTests.PipelineBehaviors;
+
+public class ValidationBehaviorTests
+{
+    [Fact]
+    public async Task ShouldBeSuccessful()
+    {
+        var logger = new Mock<ILogger<ValidationBehavior<FooRequest, Response>>>().Object;
+        var behavior = new ValidationBehavior<FooRequest, Response>([new FooRequestValidator()], logger);
+
+        var result = await behavior.Handle(
+            new FooRequest(Guid.NewGuid()), CancellationToken.None, (_, _) => ValueTask.FromResult(new Response().Success()));
+
+        result.StatusCode.ShouldBe(DefaultResponseStatusCodes.Ok);
+    }
+
+    [Fact]
+    public async Task WithNoValidator_ShouldBeSuccessful()
+    {
+        var logger = new Mock<ILogger<ValidationBehavior<FooRequest, Response>>>().Object;
+        var behavior = new ValidationBehavior<FooRequest, Response>([], logger);
+
+        var result = await behavior.Handle(
+            new FooRequest(Guid.NewGuid()), CancellationToken.None, (_, _) => ValueTask.FromResult(new Response().Success()));
+
+        result.StatusCode.ShouldBe(DefaultResponseStatusCodes.Ok);
+    }
+
+    [Fact]
+    public async Task WithInvalidRequest_ShouldReturnInvalid()
+    {
+        var logger = new Mock<ILogger<ValidationBehavior<FooRequest, Response>>>().Object;
+        var behavior = new ValidationBehavior<FooRequest, Response>([new FooRequestValidator()], logger);
+
+        var result = await behavior.Handle(
+            new FooRequest(Guid.Empty), CancellationToken.None, (_, _) => throw new InvalidOperationException());
+
+        result.StatusCode.ShouldBe(DefaultResponseStatusCodes.Invalid);
+    }
+
+    public record FooRequest(Guid Id) : IRequest<Response>;
+
+    class FooRequestValidator : AbstractValidator<FooRequest>
+    {
+        public FooRequestValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+        }
+    }
+}
