b5d2d3f2971702166a7014aadcc0f780a5893c16
Hamed Sabzian <h.sabziyan@danatadbir.ir>
2025-04-23 01:50:33 +0330

    write integration test for CreateToDoCommandHandler

---
+++ Tests/Todo.Application.IntegratedTests/CreateToDoCommandHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using Mediator;
+using Microsoft.Extensions.DependencyInjection;
+using Todo.Application.Abstraction.Interfaces;
+using Todo.Application.Commands.CreateToDo;
+using Todo.Domain.Entities;
+using Todo.Domain.Enums;
+
+namespace Todo.Application.IntegratedTests;
+
+public class CreateToDoCommandHandlerTests : IDisposable, IClassFixture<IntegratedFixture>
+{
+    private readonly IServiceScope _scope;
+    private readonly IMediator _mediator;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateToDoCommandHandlerTests(IntegratedFixture fixture)
+    {
+        _scope = fixture.Configure().CreateAsyncScope();
+        _mediator = _scope.ServiceProvider.GetRequiredService<IMediator>();
+        _unitOfWork = _scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+    }
+
+    [Fact]
+    public async Task ShouldBeSuccessful()
+    {
+        var command = new CreateToDoCommand("Foo", "Bar");
+
+        var result = await _mediator.Send(command);
+
+        result.StatusCode.ShouldBe(200);
+        var todo = await _unitOfWork.Repository<ToDo>().GetAsync(result.Data!.Value);
+        todo!.Title.ShouldBe(command.Title);
+        todo.Description.ShouldBe(command.Description);
+        todo.Status.ShouldBe(ToDoStatus.Created);
+    }
+
+    public void Dispose()
+    {
+        _scope.Dispose();
+    }
+}
---
+++ Tests/Todo.Application.IntegratedTests/IntegratedFixture.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Todo.Application.Abstraction.Interfaces;
+using Todo.Infrastructure.Data;
+
+namespace Todo.Application.IntegratedTests;
+
+public class IntegratedFixture
+{
+    public IServiceProvider Configure()
+    {
+        return Configure(_ => { });
+    }
+    public IServiceProvider Configure(Action<IServiceCollection> configureServices)
+    {
+        var services = new ServiceCollection();
+        services.AddLogging();
+
+        services.AddApplication(false);
+
+        services.AddDbContext<ToDoDbContext>(builder => { builder.UseInMemoryDatabase(Guid.NewGuid().ToString()); });
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        configureServices(services);
+
+        return services.BuildServiceProvider();
+    }
+}
