60e816fb675425e0e947cea4772a804e7367d3ee
Hamed Sabzian <h.sabziyan@danatadbir.ir>
2025-04-23 14:08:18 +0330

    write unit test for the validator of get todo query

---
+++ Tests/Todo.Application.UnitTests/Validators/GetToDoQueryValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using Todo.Application.Queries.GetToDo;
+
+namespace Todo.Application.UnitTests.Validators;
+
+public class GetToDoQueryValidatorTests
+{
+    [Fact]
+    public void ShouldBeValid()
+    {
+        var query = new GetToDoQuery(Guid.NewGuid());
+        var validator = new GetToDoQueryValidator();
+
+        var result = validator.Validate(query);
+
+        result.IsValid.ShouldBe(true);
+    }
+
+    [Fact]
+    public void WithEmptyId_ShouldBeValid()
+    {
+        var query = new GetToDoQuery(Guid.Empty);
+        var validator = new GetToDoQueryValidator();
+
+        var result = validator.Validate(query);
+
+        result.IsValid.ShouldBe(false);
+    }
+}
